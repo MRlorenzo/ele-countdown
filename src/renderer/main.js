66a630835b7535c2b96a06be5adef2bc0e0bd2e6
Lorenzo <964878800@qq.com>
2020-06-20 19:37:45 +0700

    按需引入。

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,10 +1,44 @@
 import Vue from 'vue'
-
 import App from './App'
-
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import {
+  Dialog,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
+  Form,
+  FormItem,
+  Row,
+  Col
+} from 'element-ui';
+Vue.use(Dialog);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(RadioButton);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(OptionGroup);
+Vue.use(Button);
+Vue.use(ButtonGroup);
+Vue.use(DatePicker);
+Vue.use(TimeSelect);
+Vue.use(TimePicker);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Row);
+Vue.use(Col);
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
